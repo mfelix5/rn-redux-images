dd9a83007a3314cebb2ec5f43dc447ba37200636
mfelix5 <mikefelix@gmail.com>
2019-02-03 11:15:11 -0500

    installed react-native-navigation

---
+++ src/screens/Auth/Auth.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from "react-native"
+
+export default class AuthScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Auth Screen</Text>
+            </View>
+        );
+    }
+}
